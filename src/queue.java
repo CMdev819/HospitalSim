8e2d89a4830bbf0d90352c8396fe94cf7a156bd4
W1271019 <cactusmandev@gmail.com>
2026-03-10 12:03:58 -0400

    created queue class without methods

---
+++ src/queue.java
@@ -0,0 +1,20 @@
+public class queue {
+
+    private queueRecord head; //points to beginning of queue
+    private queueRecord tail; //points to last object of queue
+
+    public void enqueue(Alert newAlert){
+        //creates new queueRecord with alert al at end of queue
+    }
+    public Alert dequeue(){
+        //moves head to refer to next item
+    }
+    public Alert peek(){
+        //returns first item in queue
+    }
+    public int count(){
+        //returns total number of items in queue
+    }
+
+
+}
